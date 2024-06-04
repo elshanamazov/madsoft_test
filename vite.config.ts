4c4b2e9e94699104e10a3254fe216ea0c7e32aa7
elshanamazov <elshanamazov@gmail.com>
2024-06-04 14:12:35 +0400

    chore: add base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/test/',
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
